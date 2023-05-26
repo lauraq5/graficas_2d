b3c7921fcff330b2e135cb12ad3c763e31f7f054
lauraq5 <lauraquintana@colegioguanenta.edu.co>
2023-05-26 07:14:54 -0500

    se modifico el archivo

--- graficas_2d.py
+++ graficas_2d.py
@@ -12,7 +12,7 @@ ALTURA = 220
 ventana_principal = Tk()
 ventana_principal.title("graficas 2D")
 ventana_principal.resizable(False, False)
-ventana_principal.geometry("800x800")
+ventana_principal.geometry("900x900")
 ventana_principal.config(bg= "white")
 
 #----------------------
@@ -52,16 +52,16 @@ frame_controles = Frame(ventana_principal)
 frame_controles.config(bg="pink2", width=480, height=230)
 frame_controles.place(x=10, y=260)
 
-# # dibujar rectangulo
+#  dibujar rectangulo
 # rect_1 = c.create_rectangle(BASE/2,ALTURA/2,BASE,ALTURA,fill="pink",outline="orange")
 # rect_2 = c.create_rectangle(0,0,BASE/2,ALTURA/2, fill="orange")
  
-#  # dibujar circulos
+# dibujar circulos
 # circ_1 = c.create_oval(BASE/2,0,BASE,ALTURA/2, fill="blue")
 # circ_2 = c.create_oval(0,ALTURA/2,BASE/4,ALTURA, fill="yellow")
 # circ_3 = c.create_oval(BASE/4,ALTURA/2,BASE/2,ALTURA, fill = "red")
 
-# # dibujar  poligonos
+# dibujar  poligonos
 # polig_1 = c.create_polygon(3*BASE/4,ALTURA/2,BASE,ALTURA, BASE/2, ALTURA, fill="red"ygon
 # polig_2= c.create_polygon(BASE/4,0,BASE/2,ALTURA/4,BASE/4,ALTURA/2,0,ALTURA/4, fill="white",outline="black")""""
 # cruz = c.create_polygon(BASE/2-25,ALTURA/2-75,BASE/2+25,ALTURA/2-25, BASE/2+75,ALTURA/2-25,BASE/2+75,ALTURA/2+25, BASE/2+25, ALTURA/2+75,BASE/2-25,ALTURA/2+75, BASE/2-25 ,ALTURA/2+25, BASE/2-25,ALTURA/2+25, BASE/2-25, ALTURA/2-25, fill="red")
@@ -69,6 +69,8 @@ frame_controles.place(x=10, y=260)
 tren = c.create_rectangle(BASE/2-30,ALTURA/2-30,BASE/2+30,ALTURA/2+30)
 tren_1 = c.create_rectangle(BASE/2-50,ALTURA/2-50,BASE/2+50,ALTURA/2+50)
 tren_2 = c.create_oval((BASE/2+20,ALTURA/2-20,BASE/2-20,ALTURA/2+20))
-tren_3 = c.create_rectangle(BASE/2+40,ALTURA/2-80,BASE/2-40,ALTURA/2-50)
-tren_4 = c.create_rectangle(BASE/2+40,ALTURA/2-70,BASE/2-30,ALTURA/2-50)
+tren_3 = c.create_rectangle(BASE/2+60,ALTURA/2-50,BASE/2-60,ALTURA/2-60)
+tren_4 = c.create_rectangle(BASE/2+40,ALTURA/2-80,BASE/2-45,ALTURA/2-60)
+tren_5 = c.create_rectangle(BASE/2+60,ALTURA/2-40,BASE/2+20,ALTURA/2-80)
+
 ventana_principal.mainloop()
