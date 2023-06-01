c183d9c74566cd6e9d8490450ffef251a7f7f2e9
lauraq5 <lauraquintana@colegioguanenta.edu.co>
2023-06-01 17:22:58 -0500

    se modifico el archivo

--- graficas_2d.py
+++ graficas_2d.py
@@ -1,76 +1,76 @@
-from tkinter import*
+import random
+from tkinter import *
 
-#--------------------
-#variables globales
-#--------------------
+import yaml
+
+# ---------------------
+# variables globales
+# ---------------------
 BASE = 460
 ALTURA = 220
+RADIO = 50
 
-#--------------------
-#ventana principal
-#--------------------
+# funcion para modificar arco
+def modificar_arco(angulo):
+      c.itemconfig(arco, extent=angulo)
+
+# --------------------
+# ventana principal
+# --------------------
 ventana_principal = Tk()
-ventana_principal.title("graficas 2D")
+ventana_principal.title("Trabajo molino")
 ventana_principal.resizable(False, False)
-ventana_principal.geometry("900x900")
-ventana_principal.config(bg= "white")
+ventana_principal.geometry("500x500")
+ventana_principal.config(bg="white")
 
-#----------------------
-#frame de graficacion
-#----------------------
+# frame de graficacion
 frame_graficacion = Frame(ventana_principal)
-frame_graficacion.config(bg="black", width=480, height=240)
-frame_graficacion.place(x=10, y=10)
+frame_graficacion.config(bg="green", width=480, height=240)
+frame_graficacion.place(x=10,y=10)
 
-#creacion canvas 
+# creacion canvas
 c = Canvas(frame_graficacion, width=BASE, height=ALTURA)
-c.config(bg="yellow")
-c.place(x=10, y=10)
+c.config(bg="black")
+c.place(x=10,y=10)
 
+# arco
+arco = c.create_arc(BASE/2-RADIO,ALTURA/2-RADIO,BASE/2+RADIO,ALTURA/2+RADIO, start=0,extent=0, fill="blue")
 
-# #dibujar una linea recta
-# linea_1 = c.create_line(BASE/2, ALTURA/2, BASE,0, fill="red", width=2)
-# linea_2 = c.create_line(BASE, ALTURA, BASE/2, ALTURA/2, fill="yellow", width=2)
-# linea_3 = c.create_line(0, ALTURA, BASE/2, ALTURA/2, fill="blue", width=2)
-# linea_4 = c.create_line(0, 0, BASE/2, ALTURA/2, fill="green", width=2)
+'''# dibujar viento 
+rect_1 = c.create_rectangle(BASE/2-70,ALTURA/2+60,BASE/2+90,ALTURA/2+40,fill="#00ffff", outline="red")
 
-# linea_1 = c.create_line(BASE/2, ALTURA/2, BASE/2, 0, fill="pink", width=2)
-# linea_2 = c.create_line(BASE/2, ALTURA/2, BASE/2, ALTURA, fill="white", width=2)
-# linea_3 = c.create_line(BASE/2, ALTURA/2, 0, ALTURA/2, fill="orange", width=2)
-# linea_4 = c.create_line(BASE/2, ALTURA/2, BASE, ALTURA/2, fill="purple", width=2)
+#dibujar poligonos
+polig_1 = c.create_polygon(3*BASE/4-105,ALTURA/2-50,BASE/2-30,ALTURA/2+40,BASE/2+50,ALTURA/2+40, fill="#ffff00")'''
 
-#dibujar texto
-# texto_1 = c.create_text(BASE/4, ALTURA/2, anchor="center", text="sistemas!!", font=("Arial", 25, "bold"), fill="blue", activefill="yellow")
-# texto_2 = c.create_text(3*BASE/4, ALTURA/2, anchor="center", text="colegio", font=("Arial", 25, "bold"), fill="blue", activefill="yellow")
-# texto_3 = c.create_text(BASE/2, ALTURA/4, anchor="center", text="especialidad", font=("Arial", 25, "bold"), fill="blue", activefill="yellow")
-# texto_2 = c.create_text(BASE/2, 3*ALTURA/4, anchor="center", text="guanenta", font=("Arial", 25, "bold"), fill="blue", activefill="yellow")
+'''# Generar 100 circulos de 20 de radio, y color y posicion aleatoria, sin salirse del canvas
+for i in range(30):
+    # generar color aleatorio
+    color = "#"
+    for i in range(6):
+            color = color + random.choice("0123456789ABCDEF")
+    # generar valor de radio, x e y aleatorio
+    radio = random.randint(5,25)
+    x = random.randint(0, BASE-2*radio)
+    y = random.randint(0,ALTURA-2*radio)
+        
+    # dibujamos el circulo
+    circulo = c.create_oval(x,y,x+2*radio,y+2*radio, fill=color)'''
 
-#---------------------
-#frame de controles
-#---------------------
+'''# agregar una imagen al canvas
+img_nave = PhotoImage(file="nave2.png")
+nave = c.create_image(BASE/2,ALTURA/2,image=img_nave)'''
+
+# ---------------------
+# frame de controles
+# ---------------------
 frame_controles = Frame(ventana_principal)
-frame_controles.config(bg="pink2", width=480, height=230)
-frame_controles.place(x=10, y=260)
+frame_controles.config(bg="green", width=480, height=230)
+frame_controles.place(x=10,y=260)
 
-#  dibujar rectangulo
-# rect_1 = c.create_rectangle(BASE/2,ALTURA/2,BASE,ALTURA,fill="pink",outline="orange")
-# rect_2 = c.create_rectangle(0,0,BASE/2,ALTURA/2, fill="orange")
- 
-# dibujar circulos
-# circ_1 = c.create_oval(BASE/2,0,BASE,ALTURA/2, fill="blue")
-# circ_2 = c.create_oval(0,ALTURA/2,BASE/4,ALTURA, fill="yellow")
-# circ_3 = c.create_oval(BASE/4,ALTURA/2,BASE/2,ALTURA, fill = "red")
+#barra de deslizamientos
+barra_deslizamiento = Scale(frame_controles, label="angulo", from_=0, to=360, orient="horizontal", length=460, tickinterval=45,
+command=modificar_arco)
+barra_deslizamiento.place(x=10,y=10)
 
-# dibujar  poligonos
-# polig_1 = c.create_polygon(3*BASE/4,ALTURA/2,BASE,ALTURA, BASE/2, ALTURA, fill="red"ygon
-# polig_2= c.create_polygon(BASE/4,0,BASE/2,ALTURA/4,BASE/4,ALTURA/2,0,ALTURA/4, fill="white",outline="black")""""
-# cruz = c.create_polygon(BASE/2-25,ALTURA/2-75,BASE/2+25,ALTURA/2-25, BASE/2+75,ALTURA/2-25,BASE/2+75,ALTURA/2+25, BASE/2+25, ALTURA/2+75,BASE/2-25,ALTURA/2+75, BASE/2-25 ,ALTURA/2+25, BASE/2-25,ALTURA/2+25, BASE/2-25, ALTURA/2-25, fill="red")
-
-tren = c.create_rectangle(BASE/2-30,ALTURA/2-30,BASE/2+30,ALTURA/2+30)
-tren_1 = c.create_rectangle(BASE/2-50,ALTURA/2-50,BASE/2+50,ALTURA/2+50)
-tren_2 = c.create_oval((BASE/2+20,ALTURA/2-20,BASE/2-20,ALTURA/2+20))
-tren_3 = c.create_rectangle(BASE/2+60,ALTURA/2-50,BASE/2-60,ALTURA/2-60)
-tren_4 = c.create_rectangle(BASE/2+40,ALTURA/2-80,BASE/2-45,ALTURA/2-60)
-tren_5 = c.create_rectangle(BASE/2+60,ALTURA/2-40,BASE/2+20,ALTURA/2-80)
-
+# desplegar ventana
 ventana_principal.mainloop()
